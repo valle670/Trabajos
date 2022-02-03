daf662862fa69a60eb9b3081fdf04dae422687a5
valle670 <98921257+valle670@users.noreply.github.com>
2022-02-02 19:42:21 -0600

    Add files via upload

---
+++ Tarea 1.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a=0,b=0, resultado1 = 0, resultado2 = 0, resultado3 = 0,resultado4 = 0;
+	
+	cout << "Ingrese el valor de a: " << endl;
+	cin >> a;
+	
+	cout << "Ingrese el valor de b: " << endl;
+	cin >> b;
+	
+	resultado1 = a + b;
+	resultado2 = a - b;
+	resultado3 = a * b;
+	resultado4 = a / b;
+	
+	cout << "la suma de a + b es: " << resultado1 << endl;
+	cout << "la suma de a - b es: " << resultado2 << endl;
+	cout << "la suma de a * b es: " << resultado3 << endl;
+	cout << "la suma de a / b es: " << resultado4 << endl;
+	
+
+
+
+
+  return 0;
+}
+
